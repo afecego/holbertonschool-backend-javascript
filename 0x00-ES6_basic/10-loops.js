9a28b5f4e08081ed83bda5215303fa80531da857
afecego <afecego@gmail.com>
2022-04-17 15:21:25 -0500

    Concaten two arrays

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arrayEnd = [];
   for (const idx of array) {
-    arrayEnd.push(`${appendString}${array[idx]}`);
+    arrayEnd.push(`${appendString}${idx}`);
   }
 
   return arrayEnd;
