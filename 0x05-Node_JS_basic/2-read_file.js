358ca2a70689f5341d024f406e663bafe36ebc0f
afecego <afecego@gmail.com>
2022-06-07 22:18:22 -0500

    Data of file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -11,7 +11,7 @@ function countStudents(file) {
     for (const line of lines) {
       if (line.trim() !== '' && i > 0) {
         countStudents += 1;
-        const [fname, lname, age, field] = line.split(',');
+        const [fname, lname, age, field] = line.split(','); // eslint-disable-line
         if (!fields[field]) {
           fields[field] = {
             count: 1,
