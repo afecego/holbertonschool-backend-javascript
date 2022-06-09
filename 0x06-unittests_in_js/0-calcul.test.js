0a1cebb2c2043607337b87086de7a7d0c8be531e
afecego <afecego@gmail.com>
2022-06-09 16:59:36 -0500

    add test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,14 @@ const assert = require('assert');
 
 describe('calculateNumber: ', function () {
 	it('...', function () {
-		result = calculateNumber(1,1);
-		assert.equal(result, 2)
+		assert.equal(calculateNumber(1, 1), 2);
+		assert.equal(calculateNumber(1, 1.2), 2);
+		assert.equal(calculateNumber(1, 1.6), 3);
+		assert.equal(calculateNumber(1.3, 1), 2);
+		assert.equal(calculateNumber(1.7, 1), 3);
+		assert.equal(calculateNumber(1.7, 1.8), 4);
+		assert.equal(calculateNumber(1.7, 1.2), 3);
+		assert.equal(calculateNumber(1.3, 1.8), 3);
+		assert.equal(calculateNumber(1.3, 1.3), 2);
 	});
 })
