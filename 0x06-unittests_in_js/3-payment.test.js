72a868961a31bf1a9237186a04c1f8981d3c1fb9
afecego <afecego@gmail.com>
2022-06-09 21:09:30 -0500

    Using sinon

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sendPaymentRequestToApi = require('./3-payment')
+const Utils = require('./utils')
+const sinon = require('sinon')
+const chai = require("chai");
+const expect = chai.expect;
+
+describe('Sure the math used', function () {
+	it('Validate the usage of the Utils function', function () {
+		const callback = sinon.spy(Utils, 'calculateNumber')
+		Utils.calculateNumber('SUM', 100, 20)
+		expect(callback.calledOnce).to.be.true;
+		expect(callback.calledWith('SUM', 100, 20)).to.be.true;
+		callback.restore()
+	})
+})
