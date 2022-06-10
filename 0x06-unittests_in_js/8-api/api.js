fa6339996e4f40c8841a71cfd6f8a69794b9d618
afecego <afecego@gmail.com>
2022-06-10 14:52:58 -0500

    fix

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
 	console.log(`API available on localhost port 7865`)
-  })
+})
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,19 +1,13 @@
+const request = require('request');
 const chai = require("chai");
 const expect = chai.expect;
 
-describe('Basic Integration testing', () => {
-	describe('GET /', () => {
-	  it('Code: 200 | Body: Welcome to the payment system', (done) => {
-		const options = {
-		  url: 'http://localhost:7865',
-		  method: 'GET',
-		};
-  
-		request(options, function (error, response, body) {
-		  expect(response.statusCode).to.equal(200);
-		  expect(body).to.equal('Welcome to the payment system');
-		  done();
-		});
-	  });
-	});
-  });
+describe('Index page', function () {
+  it('...', function (done) {
+	request('http://localhost:7865', (err, response, body) => {
+		expect(response.statusCode).to.equal(200);
+		expect(body).to.equal('Welcome to the payment system');
+		done()
+	})
+  })
+});
