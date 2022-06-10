f530548f3da2d36b069202db575d9533441ef9b0
afecego <afecego@gmail.com>
2022-06-09 22:45:30 -0500

    add before and after

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -5,22 +5,28 @@ const chai = require("chai");
 const expect = chai.expect;
 
 describe('Validate the usage of the Utils function', function () {
-	it('call sendPaymentRequestToAPI with 100, and 20', function () {
-		const api = sinon.stub(Utils, 'calculateNumber').returns(10);
-		const consol = sinon.spy(console, 'log')
-		sendPaymentRequestToApi(100, 20)
-		expect(api.calledWith('SUM', 100, 20)).to.be.true;
-		expect(consol.calledOnceWithExactly('The total is: 120'));
+	let api
+	let consol
+
+	beforeEach(function(){
+		api = sinon.stub(Utils, 'calculateNumber').returns(10);
+		consol = sinon.spy(console, 'log')
+	})
+
+	afterEach(function(){
 		api.restore()
 		consol.restore()
 	})
+
+	it('call sendPaymentRequestToAPI with 100, and 20', function () {
+		sendPaymentRequestToApi(100, 20)
+		expect(api.calledWith('SUM', 100, 20)).to.be.true;
+		expect(consol.calledOnceWithExactly('The total is: 120'));
+	})
+
 	it('call sendPaymentRequestToAPI with 10, and 10', function() {
-		const api = sinon.stub(Utils, 'calculateNumber').returns(10);
-		const consol = sinon.spy(console, 'log')
 		sendPaymentRequestToApi(10, 10)
 		expect(api.calledWith('SUM', 10, 10)).to.be.true;
 		expect(consol.calledOnceWithExactly('The total is: 20'));
-		api.restore()
-		consol.restore()
 	})
 })
