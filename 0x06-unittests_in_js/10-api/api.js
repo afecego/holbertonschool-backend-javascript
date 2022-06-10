edf9ab9bfb19d7e8de734a4c5ddff831577cdf0f
afecego <afecego@gmail.com>
2022-06-10 17:26:50 -0500

    verification get and post

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 7865;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Welcome to the payment system')
+})
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+	res.send(`Payment methods for cart ${req.params.id}`)
+})
+
+app.get('/available_payments', (req, res) => {
+	res.json({
+		payment_methods: {
+			credit_cards: true,
+			paypal: false
+		}
+	})
+})
+
+app.post('/login', (req, res) => {
+	res.send(`Welcome ${req.body.userName}`)
+})
+
+app.listen(port, () => {
+	console.log(`API available on localhost port 7865`)
+})
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,85 @@
+const request = require('request');
+const chai = require("chai");
+const expect = chai.expect;
+
+describe('Index page', function () {
+  it('...', function (done) {
+	request('http://localhost:7865', (err, response, body) => {
+		expect(response.statusCode).to.equal(200);
+		expect(body).to.equal('Welcome to the payment system');
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/cart/12', (err, response, body) => {
+		expect(response.statusCode).to.equal(200);
+		expect(body).to.equal('Payment methods for cart 12');
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/cart/hello', (err, response, body) => {
+		expect(response.statusCode).to.equal(404);
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/cart/a1', (err, response, body) => {
+		expect(response.statusCode).to.equal(404);
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/cart/1a', (err, response, body) => {
+		expect(response.statusCode).to.equal(404);
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/cart/1a1', (err, response, body) => {
+		expect(response.statusCode).to.equal(404);
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/cart/a1b', (err, response, body) => {
+		expect(response.statusCode).to.equal(404);
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/cart/', (err, response, body) => {
+		expect(response.statusCode).to.equal(404);
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/available_payments', (err, response, body) => {
+		expect(response.statusCode).to.equal(200);
+		expect(body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/available_payments', (err, response, body) => {
+		expect(response.statusCode).to.equal(200);
+		const bodyJs = JSON.parse(body);
+
+        const bodyGet = {
+          payment_methods: {
+            credit_cards: true,
+            paypal: false,
+          },
+        };
+		expect(bodyJs).to.deep.equal(bodyGet);
+		done()
+	})
+  })
+  it('...', function (done) {
+	request('http://localhost:7865/available_payments', 'POST', (err, response, body) => {
+		expect(response.statusCode).to.equal(200);
+		expect(body).to.equal('Welcome undefined');
+		done()
+	})
+  })
+});
