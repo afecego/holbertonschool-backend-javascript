cc0c364df2a52b98b3dcaf76f76efd0074fa441a
afecego <afecego@gmail.com>
2022-06-09 21:18:04 -0500

    fix

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,7 +7,7 @@ const expect = chai.expect;
 describe('Sure the math used', function () {
 	it('Validate the usage of the Utils function', function () {
 		const callback = sinon.spy(Utils, 'calculateNumber')
-		Utils.calculateNumber('SUM', 100, 20)
+		sendPaymentRequestToApi(100, 20);
 		expect(callback.calledOnce).to.be.true;
 		expect(callback.calledWith('SUM', 100, 20)).to.be.true;
 		callback.restore()
